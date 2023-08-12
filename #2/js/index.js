d833eea28ba0e92841346153474fc7ec5fa13b12
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-08-11 23:38:58 -0300

    UP

---
+++ #2/js/index.js
@@ -0,0 +1,42 @@
+let changeMenu = document.getElementById("content")
+let table1 = document.getElementById("table")
+let table2 = document.getElementById("table2")
+let table3 = document.getElementById("table3")
+let container = document.getElementById("container")
+let changeMenu2 = document.getElementById("content2")
+
+changeMenu.addEventListener("click", function(){
+    table3.style.animation = "animation 0.2s"
+    setTimeout(() => {
+        table2.style.display = "none"
+        table3.style.top = "50%"
+        table3.style.left = "38%"
+        table3.style.transform = "translate(-50%, -50%)"
+        table3.style.animation = "animation2L 0.2s"
+        table3.style.transform = "rotate(45deg)"
+    }, 150)
+    
+    table1.style.animation = "animation 0.2s"
+    setTimeout(() => {
+        table1.style.top = "50%"
+        table1.style.left = "38%"
+        table1.style.transform = "translate(-50%, -50%)"     
+        table1.style.animation = "animation2R 0.2s"
+        table1.style.transform = "rotate(-45deg)"       
+    }, 150)
+    this.addclass
+})
+
+changeMenu2.addEventListener("click", function(){
+    table2.style.display = ""
+    table3.style.transform = "rotate(0deg)"
+    table3.style.top = "45%"
+    table3.style.left = "50%"
+    table3.style.transform = "translate(-50%, -50%)"   
+    
+    table1.style.transform = "rotate(0deg)"   
+    table1.style.top = "55%"
+    table1.style.left = "50%"
+    table1.style.transform = "translate(-50%, -50%)"   
+    console.log("oi")
+})
