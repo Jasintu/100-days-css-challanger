d5699c9d9aa9d3d20bccc9c9bf9a245389a72ea2
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-08-12 00:14:27 -0300

    UP

--- #2/js/index.js
+++ #2/js/index.js
@@ -5,38 +5,46 @@ let table3 = document.getElementById("table3")
 let container = document.getElementById("container")
 let changeMenu2 = document.getElementById("content2")
 
-changeMenu.addEventListener("click", function(){
-    table3.style.animation = "animation 0.2s"
-    setTimeout(() => {
-        table2.style.display = "none"
-        table3.style.top = "50%"
-        table3.style.left = "38%"
-        table3.style.transform = "translate(-50%, -50%)"
-        table3.style.animation = "animation2L 0.2s"
-        table3.style.transform = "rotate(45deg)"
-    }, 150)
-    
-    table1.style.animation = "animation 0.2s"
-    setTimeout(() => {
-        table1.style.top = "50%"
-        table1.style.left = "38%"
-        table1.style.transform = "translate(-50%, -50%)"     
-        table1.style.animation = "animation2R 0.2s"
-        table1.style.transform = "rotate(-45deg)"       
-    }, 150)
-    this.addclass
-})
+var set = 1;
 
-changeMenu2.addEventListener("click", function(){
-    table2.style.display = ""
-    table3.style.transform = "rotate(0deg)"
-    table3.style.top = "45%"
-    table3.style.left = "50%"
-    table3.style.transform = "translate(-50%, -50%)"   
-    
-    table1.style.transform = "rotate(0deg)"   
-    table1.style.top = "55%"
-    table1.style.left = "50%"
-    table1.style.transform = "translate(-50%, -50%)"   
-    console.log("oi")
+changeMenu.addEventListener("click", function(){
+    if(set == 1){
+
+        set = 2;
+        table3.style.animation = "animation 0.2s"
+        setTimeout(() => {
+            table2.style.display = "none"
+            table3.style.top = "50%"
+            table3.style.left = "38%"
+            table3.style.transform = "translate(-50%, -50%)"
+            table3.style.animation = "animation2L 0.2s"
+            table3.style.transform = "rotate(45deg)"
+        }, 150)
+        
+        table1.style.animation = "animation 0.2s"
+        setTimeout(() => {
+            table1.style.top = "50%"
+            table1.style.left = "38%"
+            table1.style.transform = "translate(-50%, -50%)"     
+            table1.style.animation = "animation2R 0.2s"
+            table1.style.transform = "rotate(-45deg)"       
+        }, 150)
+
+    } else if (set === 2) {
+
+        set = 1;
+        table2.style.display = ""
+        table3.style.animation = "animation3R 0.2s"
+        table3.style.transform = "rotate(0deg)"
+        table3.style.top = "45%"
+        table3.style.left = "50%"
+        table3.style.transform = "translate(-50%, -50%)"   
+        
+        table1.style.animation = "animation3L 0.2s"
+        table1.style.transform = "rotate(0deg)"   
+        table1.style.top = "55%"
+        table1.style.left = "50%"
+        table1.style.transform = "translate(-50%, -50%)"   
+
+    }
 })
